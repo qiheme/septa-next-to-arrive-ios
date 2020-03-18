b9d957610fd496ccf2831ea9d1e36b9454256903
Quincy Iheme <qiheme@gmail.com>
2020-03-18 13:39:36 -0400

    Add dropdown UIPicker for Starting station

--- SEPTANextToArrive/ViewController.swift
+++ SEPTANextToArrive/ViewController.swift
@@ -8,7 +8,168 @@
 
 import UIKit
 
-class ViewController:UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    @IBOutlet weak var startingStaion: UITextField!
+    @IBOutlet weak var endingStationPicker: UIPickerView!
+    
+    var selectedStation: String?
+    var stationList = ["30th Street Station",
+       "49th Street",
+       "9th Street",
+       "Airport Terminal A",
+       "Airport Terminal B",
+       "Airport Terminals C & D",
+       "Airport Terminals E & F",
+       "Allegheny",
+       "Allen Lane",
+       "Ambler",
+       "Angora",
+       "Ardmore",
+       "Ardsley",
+       "Bala",
+       "Berwyn",
+       "Bethayres",
+       "Bridesburg",
+       "Bristol",
+       "Bryn Mawr",
+       "Carpenter",
+       "Chalfont",
+       "Chelten Avenue",
+       "Cheltenham",
+       "Chester Transportation Center",
+       "Chestnut Hill East",
+       "Chestnut Hill West",
+       "Churchmans Crossing",
+       "Claymont",
+       "Clifton–Aldan",
+       "Colmar",
+       "Conshohocken",
+       "Cornwells Heights",
+       "Crestmont",
+       "Croydon",
+       "Crum Lynne",
+       "Curtis Park",
+       "Cynwyd",
+       "Darby",
+       "Daylesford",
+       "Delaware Valley University",
+       "Devon",
+       "Downingtown",
+       "Doylestown",
+       "East Falls",
+       "Eastwick",
+       "Eddington",
+       "Eddystone",
+       "Elkins Park",
+       "Elm Street",
+       "Elwyn",
+       "Exton",
+       "Fern Rock Transportation Center",
+       "Fernwood–Yeadon",
+       "Folcroft",
+       "Forest Hills",
+       "Fort Washington",
+       "Fortuna",
+       "Fox Chase",
+       "Germantown",
+       "Gladstone",
+       "Glenolden",
+       "Glenside",
+       "Gravers",
+       "Gwynedd Valley",
+       "Hatboro",
+       "Haverford",
+       "Highland Avenue",
+       "Highland",
+       "Holmesburg Junction",
+       "Ivy Ridge",
+       "Jefferson Station",
+       "Jenkintown–Wyncote",
+       "Langhorne",
+       "Lansdale",
+       "Lansdowne",
+       "Lawndale",
+       "Levittown",
+       "Link Belt",
+       "Main Street",
+       "Malvern",
+       "Manayunk",
+       "Marcus Hook",
+       "Meadowbrook",
+       "Media",
+       "Melrose Park",
+       "Merion",
+       "Miquon",
+       "Morton",
+       "Mount Airy",
+       "Moylan–Rose Valley",
+       "Narberth",
+       "Neshaminy Falls",
+       "New Britain",
+       "Newark",
+       "Noble",
+       "Norristown Transportation Center",
+       "North Broad",
+       "North Hills",
+       "North Philadelphia",
+       "North Wales",
+       "Norwood",
+       "Olney",
+       "Oreland",
+       "Overbrook",
+       "Paoli",
+       "Penllyn",
+       "Penn Medicine",
+       "Pennbrook",
+       "Philmont",
+       "Primos",
+       "Prospect Park",
+       "Queen Lane",
+       "Radnor",
+       "Ridley Park",
+       "Rosemont",
+       "Roslyn",
+       "Rydal",
+       "Ryers",
+       "Secane",
+       "Sedgwick",
+       "Sharon Hill",
+       "Somerton",
+       "Spring Mill",
+       "St. Davids",
+       "St. Martins",
+       "Stenton",
+       "Strafford",
+       "Suburban Station",
+       "Swarthmore",
+       "Tacony",
+       "Temple University",
+       "Thorndale",
+       "Torresdale",
+       "Trenton Transit Center",
+       "Trevose",
+       "Tulpehocken",
+       "Upsal",
+       "Villanova",
+       "Wallingford",
+       "Warminster",
+       "Washington Lane",
+       "Wayne Junction",
+       "Wayne",
+       "West Trenton",
+       "Whitford",
+       "Willow Grove",
+       "Wilmington",
+       "Wissahickon",
+       "Wister",
+       "Woodbourne",
+       "Wyndmoor",
+       "Wynnefield Avenue",
+       "Wynnewood",
+       "Yardley"
+   ]
+
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -19,7 +180,7 @@ class ViewController:UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
      
      // The data to return fopr the row and component (column) that's being passed in
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return stations[row]
+        return stationList[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
@@ -27,20 +188,37 @@ class ViewController:UIViewController, UIPickerViewDelegate, UIPickerViewDataSou
         // The parameter named row and component represents what was selected.
         print(row)
         print(component)
+        selectedStation = stationList[row] // selected item
+        startingStaion.text = selectedStation
     }
 
-    @IBOutlet weak var startingStationPicker: UIPickerView!
-    @IBOutlet weak var endingStationPicker: UIPickerView!
+    func createPickerView() {
+           let pickerView = UIPickerView()
+           pickerView.delegate = self
+           startingStaion.inputView = pickerView
+    }
+
+    func dismissPickerView() {
+       let toolBar = UIToolbar()
+       toolBar.sizeToFit()
+       let button = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.action))
+       toolBar.setItems([button], animated: true)
+       toolBar.isUserInteractionEnabled = true
+       startingStaion.inputAccessoryView = toolBar
+    }
+
+    @objc func action() {
+          view.endEditing(true)
+    }
     
     var stations: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        self.startingStationPicker.delegate = self
-        self.startingStationPicker.dataSource = self
-        
+        createPickerView()
+        dismissPickerView()
+
         self.endingStationPicker.delegate = self
         self.endingStationPicker.dataSource = self
 
