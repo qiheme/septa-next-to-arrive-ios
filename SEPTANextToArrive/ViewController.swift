3a9804155334dbb49308131fdabf25c4e4a6298f
Quincy Iheme <qiheme@gmail.com>
2020-03-18 09:20:38 -0400

    Initial Commit

---
+++ SEPTANextToArrive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SEPTANextToArrive
+//
+//  Created by Iheme, Quincy on 3/18/20.
+//  Copyright © 2020 Iheme, Quincy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
